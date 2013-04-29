4fa22267b2baaadca6698639da8780e3ec3ccc82
Jordon Phillips <phillijk@rose-hulman.edu>
2013-04-28 21:59:38 -0400

    Added neo4j 2.0 dependencies

--- Babel/src/babel/Main.java
+++ Babel/src/babel/Main.java
@@ -1,8 +1,28 @@
 package babel;
 
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String DB_PATH = "";
+        GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( DB_PATH );
+        registerShutdownHook(graphDb);
+    }
+
+    private static void registerShutdownHook( final GraphDatabaseService graphDb )
+    {
+        // Registers a shutdown hook for the Neo4j instance so that it shuts down
+        // nicely when the VM exits (even if you "Ctrl-C" the running example
+        // before it's completed)
+        Runtime.getRuntime().addShutdownHook(new Thread()
+        {
+            @Override
+            public void run()
+            {
+                graphDb.shutdown();
+            }
+        });
     }
 }
