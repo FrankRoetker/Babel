7a1dc781f23d7540f9a1a3d24545e67d001804e3
Jordon Phillips <phillijk@rose-hulman.edu>
2013-04-28 22:40:00 -0400

    Successuflly configured and connected to our server.

--- Babel/src/babel/Main.java
+++ Babel/src/babel/Main.java
@@ -1,14 +1,20 @@
 package babel;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 public class Main {
 
     public static void main(String[] args) {
-        String DB_PATH = "";
-        GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( DB_PATH );
-        registerShutdownHook(graphDb);
+        String path = "http://babel.csse.rose-hulman.edu:7474";
+        WebResource resource = Client.create().resource(path);
+        ClientResponse response = resource.get(ClientResponse.class);
+
+        System.out.println(String.format("GET on [%s], status code [%d]", path, response.getStatus()));
+        response.close();
     }
 
     private static void registerShutdownHook( final GraphDatabaseService graphDb )
